496e68745ebc8c56ed5f24d78a698dcfb556aead
Rahul Dey <rahuldey832000@gmail.com>
2026-04-04 07:54:54 +0530

    Upgrade python version from 3.12 to 3.13

--- src/main.py
+++ src/main.py
@@ -1,6 +1,1 @@
-import streamlit as st
-
-st.header("Document Processing RAG Application 🚀")
-st.write(
-    "Process multiple documents like pdf, image or urls and chat with this documents by using LLM 🎉"
-)
+print("Hello from document rag")
